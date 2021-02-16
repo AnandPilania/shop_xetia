1461ac22134018137155724919014f7f1a51404a
sniren-ren <53244523+sniren210@users.noreply.github.com>
2021-02-17 00:02:48 +0700

    fix error di add product

--- lib/networks/product_service/product.dart
+++ lib/networks/product_service/product.dart
@@ -37,31 +37,30 @@ class Product {
 
   Future<AddProductResponse> postProduct() async {
     try {
-      http.Response res =
-          await http.post("$productService/api/v1/product/store",
-              headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json",
-                "Authorization":
-                    "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjEzNjU5NjM4LCJqdGkiOiI0NjgzODFlYmU5MTQ0YTBlODFmZmJhZThmMjI1ZDlmZSIsInVzZXJfaWQiOiJlY2ViYmUwNy1lNGFiLTRlOWItYjljYy1mMjkxYmNlYjAxZTMifQ.nr6Ephl-HVa9FTR9nagnmOCoYSBCblUcLqD2t4fYU4A"
-              },
-              body: jsonEncode({
-                "name": "example",
-                "description": "example description",
-                "weight": "120gram",
-                "thumbnail":
-                    "https://media.suara.com/pictures/480x260/2020/05/28/40379-sajian-mie-ayam-bakso-shutterstock.jpg",
-                "category": "71088502-e2c5-4c66-a305-84064e30f48b",
-                "discount": "0",
-                "images": [
-                  "https://media.suara.com/pictures/480x260/2020/05/28/40379-sajian-mie-ayam-bakso-shutterstock.jpg"
-                ],
-                "price": "1000",
-                "brand": "example brand",
-                "quantity": "0",
-                "shop": "b31f781c-5ccb-4e97-a0ee-c0d93fa8f446",
-                "user": "59b0fa92-239f-42b3-be8d-949a58b09986"
-              }));
+      http.Response res = await http.post("$baseUrl/api/v1/product/store",
+          headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization":
+                "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjEzNjU5NjM4LCJqdGkiOiI0NjgzODFlYmU5MTQ0YTBlODFmZmJhZThmMjI1ZDlmZSIsInVzZXJfaWQiOiJlY2ViYmUwNy1lNGFiLTRlOWItYjljYy1mMjkxYmNlYjAxZTMifQ.nr6Ephl-HVa9FTR9nagnmOCoYSBCblUcLqD2t4fYU4A"
+          },
+          body: jsonEncode({
+            "name": "example",
+            "description": "example description",
+            "weight": "120gram",
+            "thumbnail":
+                "https://media.suara.com/pictures/480x260/2020/05/28/40379-sajian-mie-ayam-bakso-shutterstock.jpg",
+            "category": "71088502-e2c5-4c66-a305-84064e30f48b",
+            "discount": "0",
+            "images": [
+              "https://media.suara.com/pictures/480x260/2020/05/28/40379-sajian-mie-ayam-bakso-shutterstock.jpg"
+            ],
+            "price": "1000",
+            "brand": "example brand",
+            "quantity": "0",
+            "shop": "b31f781c-5ccb-4e97-a0ee-c0d93fa8f446",
+            "user": "59b0fa92-239f-42b3-be8d-949a58b09986"
+          }));
 
       if (res.statusCode == 200) {
         AddProductResponse decode = addProductResponseFromJson(res.body);
