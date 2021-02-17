c08b5959990220123be8b1fa36da3e5c6dc99083
Adithya-13 <aditputrafirmansyah@gmail.com>
2021-02-17 20:51:23 +0700

    add back button

--- lib/ui/components/detail_sheet/detail_header.dart
+++ lib/ui/components/detail_sheet/detail_header.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:xetia_shop/constants/_constants.dart';
 import 'package:get/get.dart';
+import 'package:xetia_shop/constants/_constants.dart';
 
 class DetailHeader extends StatelessWidget {
   const DetailHeader({
@@ -13,10 +14,28 @@ class DetailHeader extends StatelessWidget {
       height: heightApp * 0.075,
       width: widthApp,
       color: context.theme.primaryColor,
-      child: Center(
-          child: Text("Product Details",
-              style: context.textTheme.headline3
-                  .copyWith(fontWeight: FontWeight.bold))),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            icon: Icon(CupertinoIcons.back),
+            onPressed: () {
+              Get.back();
+            },
+          ),
+          Expanded(
+            child: Center(
+              child: Text("Product Details",
+                  textAlign: TextAlign.center,
+                  style: context.textTheme.headline3
+                      .copyWith(fontWeight: FontWeight.bold)),
+            ),
+          ),
+          SizedBox(
+            width: 48,
+          ),
+        ],
+      ),
     );
   }
 }
